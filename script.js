e70ef867ceba9eec7052b5a8cc7edfe912c9f746
Пользователь <kananhajizada98@gmail.com>
2020-01-23 19:45:14 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function Azn(){
+    let a = document.getElementById("azn").value ;
+   document.getElementById("usd").value = a / 1.7 ;
+    document.getElementById("eur").value = a / 1.872 ;
+    document.getElementById("tl").value = a * 3.39 ;
+ 
+ }
+ 
+ 
